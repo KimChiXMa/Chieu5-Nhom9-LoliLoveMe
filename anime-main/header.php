ddd85fac5ee16e7a01cdca8917838c5ae81992e6
ThienLoliKon <23211TT4279@mail.tdc.edu.vn>
2024-12-11 16:10:41 +0700

    fix bug ver1.0

--- anime-main/header.php
+++ anime-main/header.php
@@ -4,6 +4,7 @@ require "config.php";
 require "models/db.php";
 require "models/anime.php";
 require "models/item.php";
+require "models/anime_tag.php";
 
 $anime = new Anime;?>
 <header class="header">
